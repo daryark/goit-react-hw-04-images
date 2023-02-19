13fc29d0ac73e5cd5cfe5a9cc627fa63364580d6
Dasha Yarkovska <yarkovska11@gmail.com>
2023-02-19 20:03:18 +0200

    fix props

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -21,11 +21,11 @@ export const ImageGallery = ({ photos, openModal }) => {
 ImageGallery.propTypes = {
   photos: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
+      id: PropTypes.number,
+      webformatURL: PropTypes.string,
+      largeImageURL: PropTypes.string,
+      tags: PropTypes.string,
     })
-  ).isRequired,
+  ),
   openModal: PropTypes.func.isRequired,
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,24 +4,23 @@ import { Backdrop, Modal } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export function ModalWindow() {
+export function ModalWindow({ modalData, closeModal }) {
   useEffect(() => {
-    window.addEventListener('keydown', this.closeOnEscape);
+    window.addEventListener('keydown', closeOnEscape);
     return () => {
-      window.removeEventListener('keydown', this.closeOnEscape);
+      window.removeEventListener('keydown', closeOnEscape);
     };
-  }, []);
+  });
 
   const closeOnEscape = e => {
-    if (e.code === 'Escape') this.props.closeModal();
+    if (e.code === 'Escape') closeModal();
   };
 
   const closeOnBackdrop = e => {
-    if (e.target === e.currentTarget) this.props.closeModal();
+    if (e.target === e.currentTarget) closeModal();
   };
 
-  const { largeImg, tag } = this.props.modalData;
-
+  const { largeImg, tag } = modalData;
   return createPortal(
     <Backdrop onClick={closeOnBackdrop}>
       <Modal onKeyDown={closeOnEscape}>
